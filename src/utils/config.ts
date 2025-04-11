1a8db5f7bea6fe62c246d9c7cf84468f20f56727
Michael Turck <michaelrturck@gmail.com>
2025-04-10 19:58:10 -0700

    chore: sync SDK from main repository

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -3,7 +3,7 @@ import { SDKConfig } from "../types/sdk";
 export const defaultConfig: SDKConfig = {
   logLevel: "verbose",
   explorer: "solscan",
-  ammProgramPath: "../../target/idl/amm.json",
-  token2022ProgramPath: "../../target/idl/token_2022_factory.json",
-  splTokenProgramPath: "../../target/idl/spl_token_factory.json",
+  ammProgramPath: "../target/idl/amm.json",
+  token2022ProgramPath: "../target/idl/token_2022_factory.json",
+  splTokenProgramPath: "../target/idl/spl_token_factory.json",
 };
