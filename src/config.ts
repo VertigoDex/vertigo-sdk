b06454add2d86c6eb29617529bcacbc01309e3e4
Michael Turck <michaelrturck@gmail.com>
2025-04-29 14:41:27 -0400

    chore: sync SDK from main repository

--- src/config.ts
+++ src/config.ts
@@ -2,13 +2,7 @@ import { SDKConfig } from "./types/sdk";
 import { getClusterFromEndpoint, getExplorerUrl } from "./utils/helpers";
 import * as anchor from "@coral-xyz/anchor";
 import { defaultConfig } from "./utils/config";
-import { Connection, PublicKey } from "@solana/web3.js";
-import { AnchorProvider } from "@coral-xyz/anchor";
-import {
-  isWalletAdapter,
-  toAnchorProvider,
-  AdapterWithTx,
-} from "./utils/bridge";
+import { Connection } from "@solana/web3.js";
 
 export class VertigoConfig {
   public provider: anchor.AnchorProvider;
@@ -18,12 +12,8 @@ export class VertigoConfig {
   public explorer: "solscan" | "solanaExplorer";
   public cluster: string;
 
-  public ammProgramId: PublicKey;
-  public splTokenFactoryProgramId: PublicKey;
-  public token2022FactoryProgramId: PublicKey;
-
   constructor(
-    provider: anchor.AnchorProvider | AdapterWithTx,
+    provider: anchor.AnchorProvider,
     config: SDKConfig = defaultConfig
   ) {
     const sdkConfig = {
@@ -31,31 +21,14 @@ export class VertigoConfig {
       ...config,
     };
 
-    const _provider: AnchorProvider = isWalletAdapter(provider)
-      ? toAnchorProvider(
-          provider as AdapterWithTx,
-          new Connection(provider.url ?? "https://api.mainnet-beta.solana.com"),
-          {
-            commitment: "confirmed",
-          }
-        )
-      : provider;
-    this.provider = _provider;
-    this.connection = _provider.connection;
+    this.provider = provider;
+    this.connection = provider.connection;
 
     this.logLevel = sdkConfig?.logLevel || defaultConfig.logLevel;
     this.explorer = sdkConfig?.explorer || defaultConfig.explorer;
-    this.cluster = getClusterFromEndpoint(_provider.connection.rpcEndpoint);
+    this.cluster = getClusterFromEndpoint(provider.connection.rpcEndpoint);
 
     anchor.setProvider(this.provider);
-
-    this.ammProgramId = new PublicKey(sdkConfig.ammProgramId);
-    this.splTokenFactoryProgramId = new PublicKey(
-      sdkConfig.splTokenFactoryProgramId
-    );
-    this.token2022FactoryProgramId = new PublicKey(
-      sdkConfig.token2022FactoryProgramId
-    );
   }
 
   public logTx(signature: string, operation: string) {
